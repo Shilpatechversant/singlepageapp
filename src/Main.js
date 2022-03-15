e0e1f133c4df2e6a97ab5005c4aaae9ec2e797a1
Shilpatechversant <shilpa@techversantinfo.com>
2022-03-15 16:18:05 +0530

    message

---
+++ src/Main.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+    BrowserRouter,
+    Routes,
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+ 
+class Main extends Component {
+    render() {
+        return (
+            <BrowserRouter>        
+         
+            <div>
+              <h1>Simple SPA</h1>
+              <ul className="header">
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/stuff">Stuff</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+              </ul>
+              <div className="content">
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/stuff" element={<Stuff />} />
+                <Route path="/contact" element={<Contact />}/>
+                </Routes>
+              </div>
+            </div>
+        
+       
+  </BrowserRouter>
+        );
+      }
+}
+ 
+export default Main;
